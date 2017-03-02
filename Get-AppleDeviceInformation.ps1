fb67cdcca45994c00a67c1f7d1b5b71e77cc4915
Anders Wahlqvist <anders.wahlqvist@hotmail.com>
2017-03-02 13:20:27 +0100

    Update Get-AppleDeviceInformation.ps1
    Updated for Apples new api

--- Get-AppleDeviceInformation.ps1
+++ Get-AppleDeviceInformation.ps1
@@ -6,55 +6,62 @@ function Get-AppleDeviceInformation
           [System.Management.Automation.PSCredential] $Credential,
           [switch] $WaitForLocationFix = $true)
 
-    $clientBuildNumber = '15D108'
-    $clientId = '000110-10-861fd091-b128-4525-8023-7f1ee59cff89'
 
-
-    $LoginUri = "https://setup.icloud.com/setup/ws/1/login?clientBuildNumber=$clientBuildNumber&clientId=$clientId"
-
-
-    $LoginParameters = @{
-                            clientBuildNumber = $clientBuildNumber
-                            clientId = $clientId
-                        }
+    $clientBuildNumber = '17AProject83'
+    $clientMasteringNumber = '17A77'
+    $clientId = '90E1837A-5A86-4FAB-B141-5841BEEEB06E'
+    $LoginDataFetchUri = "https://setup.icloud.com/setup/ws/1/login?clientBuildNumber=$clientBuildNumber&clientId=$clientId"
 
     $PayloadHash = @{
-                        apple_id = $Credential.UserName
-                        extended_login = $false
-                        password = $Credential.GetNetworkCredential().Password
-                    }
+        'apple_id' = $Credential.UserName
+        'extended_login' = $false
+        'password' = $Credential.GetNetworkCredential().Password
+    }
 
-    $PayloadJsonObj = $PayloadHash | ConvertTo-Json
+    $PayloadJsonObj = $PayloadHash | ConvertTo-Json -Compress
 
     $Header = @{
                 'Origin' = 'https://www.icloud.com'
-                'Referer' = 'https://www.icloud.com'
+                'Referer' = 'https://www.icloud.com/'
                }
 
-
-    $LoginData = Invoke-RestMethod -Uri $LoginUri -Body $PayloadJsonObj -Headers $Header -Method Post -SessionVariable iCloudSession -UserAgent ([Microsoft.PowerShell.Commands.PSUserAgent]::Chrome)
-
-    $FindMyiPhoneURI = "https://p36-fmipweb.icloud.com/fmipservice/client/web/refreshClient?clientBuildNumber=$clientBuildNumber&clientId=$($LoginData.dsInfo.aDsID)&dsid=$($LoginData.dsInfo.dsid)"
-    # $FindMyiPhoneURI = "https://p02-fmipweb.icloud.com/fmipservice/client/web/refreshClient?clientBuildNumber=$clientBuildNumber&clientId=$clientId&dsid=$($LoginData.dsInfo.dsid)"
+    try {
+        $LoginData = Invoke-RestMethod -Uri $LoginDataFetchUri -Body $PayloadJsonObj -Headers $Header -Method Post -SessionVariable iCloudSession -UserAgent ([Microsoft.PowerShell.Commands.PSUserAgent]::Chrome) -ErrorAction Stop
+    }
+    catch {
+        throw "Loggin failed. Error: $($_.ToString())"
+    }
 
     $LocationPayload = @{
                            clientContext = @{
-                                apiVersion = '3.0'
-                                appName = 'iCloud Find (Web)'
-                                appVersion = '2.0'
-                                fmly = $true
-                                inactiveTime = '2191'
-                                timezone = 'Europe/Stockholm'
+                                appVersion = '1.0'
+                                contextApp = 'com.icloud.web.fmf'
+                                mapkitAvailable = $true
+                                productType = 'fmfweb'
+                                tileServer = 'Apple'
+                                userInactivityTimeInMS = 20
+                                windowInFocus = $true
+                                windowVisible = $true
                             }
                         }
 
-    $LocationPayloadJsonObj = $LocationPayload | ConvertTo-Json
+    $LocationPayloadJsonObj = $LocationPayload | ConvertTo-Json -Compress
+
+   $LocationFetchHeaders = @{
+                'Origin' = 'https://www.icloud.com'
+                'Referer' = 'https://www.icloud.com/'
+               }
+    $LocationURI = "$($LoginData.webservices.fmf.url)/fmipservice/client/fmfweb/refreshClient?clientBuildNumber=$clientBuildNumber&clientId=$($LoginData.dsInfo.aDsID)&clientMasteringNumber=$clientMasteringNumber&dsid=$($LoginData.dsInfo.dsid)"
 
     $WaitingForLocationFix = $true
+    $MaxTries = 5
+    $AttemptNr = 0
 
-    while ($WaitingForLocationFix) {
+    while ($WaitingForLocationFix -and $AttemptNr -lt $MaxTries) {
+        $AttemptNr++
 
-        $LocationPostResults = Invoke-RestMethod -Uri $FindMyiPhoneURI -Body $LocationPayloadJsonObj -Method Post -WebSession $iCloudSession -UserAgent ([Microsoft.PowerShell.Commands.PSUserAgent]::Chrome)
+        #$LoginData = Invoke-RestMethod -Uri $LoginDataFetchUri -Body $PayloadJsonObj -Headers $Header -Method Post -SessionVariable iCloudSession -UserAgent ([Microsoft.PowerShell.Commands.PSUserAgent]::Chrome)
+        $LocationPostResults = Invoke-RestMethod -Uri $LocationURI -Body $LocationPayloadJsonObj -Method Post -Headers $LocationFetchHeaders -WebSession $iCloudSession -UserAgent ([Microsoft.PowerShell.Commands.PSUserAgent]::Chrome)
         
         foreach ($iOSDevice in $LocationPostResults.content) {
             if ($iOSDevice.deviceStatus -ne 200) {
@@ -83,5 +90,9 @@ function Get-AppleDeviceInformation
         else {
             $WaitingForLocationFix = $false
         }
+    }
+
+    if ($WaitingForLocationFix) {
+        throw "Timed out waiting for a location fix."
     }
 }
